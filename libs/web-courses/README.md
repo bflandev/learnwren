309f07569adf610c6cef94ddb25c3ed646f396df
Brian Flanagan <bflan@Brians-Mac-mini.fios-router.home>
2026-05-13 01:04:58 -0400

    feat(web-courses): scaffold Angular lib + add @angular/cdk for DragDrop

---
+++ libs/web-courses/README.md
@@ -0,0 +1,7 @@
+# web-courses
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test web-courses` to execute the unit tests.
---
+++ libs/web-courses/src/lib/web-courses/web-courses.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-web-courses',
+  imports: [],
+  templateUrl: './web-courses.html',
+  styleUrl: './web-courses.css',
+})
+export class WebCourses {}
